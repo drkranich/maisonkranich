f585c5676ff95ab78d53932baf873d9a35a764c5
drkranich <proxymovelfads@gmail.com>
2026-06-22 17:00:38 -0300

    ok

--- open-next.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-// Configuração do adapter OpenNext para Cloudflare Workers.
-// Cache pode ser adicionado depois (R2/KV): https://opennext.js.org/cloudflare/caching
-export default defineCloudflareConfig();
